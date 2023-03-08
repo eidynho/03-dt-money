fdadeb6a563770ceb59040a3448cddd264367c7f
eidy <zviniciuseidy2@gmail.com>
2023-03-08 13:36:30 -0300

    new: use package use-context-selector to prevent update components adrift

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,9 +1,9 @@
+import { useContextSelector } from "use-context-selector";
 import { MagnifyingGlass } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
 import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
@@ -13,7 +13,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-    const { fetchTransactions } = useContext(TransactionsContext);
+    const fetchTransactions = useContextSelector(
+        TransactionsContext,
+        (context) => {
+            return context.fetchTransactions;
+        },
+    );
 
     const {
         register,
