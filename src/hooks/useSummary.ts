2df3426db039f4b2eb3ab3c4c7a8e38a5db9ed6e
eidy <zviniciuseidy2@gmail.com>
2023-03-08 14:00:03 -0300

    new: useMemo to prevent const summary recalculcation without any meaningful change

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,5 @@
+import { useMemo } from "react";
 import { useContextSelector } from "use-context-selector";
-
 import { TransactionsContext } from "../contexts/TransactionsContext";
 
 export function useSummary() {
@@ -7,24 +7,26 @@ export function useSummary() {
         return context.transactions;
     });
 
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === "income") {
-                acc.income += transaction.price;
-                acc.total += transaction.price;
-            } else {
-                acc.outcome += transaction.price;
-                acc.total -= transaction.price;
-            }
+    const summary = useMemo(() => {
+        return transactions.reduce(
+            (acc, transaction) => {
+                if (transaction.type === "income") {
+                    acc.income += transaction.price;
+                    acc.total += transaction.price;
+                } else {
+                    acc.outcome += transaction.price;
+                    acc.total -= transaction.price;
+                }
 
-            return acc;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0,
-        },
-    );
+                return acc;
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0,
+            },
+        );
+    }, [transactions]);
 
     return summary;
 }
